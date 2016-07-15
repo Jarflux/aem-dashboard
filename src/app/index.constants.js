30e1bd9f2337dce8d1c8b7f83dcda46f84deb631
Mathieu De Haeck <mathieu.de.haeck@gmail.com>
2016-07-15 13:55:27 +0200

    angular-gulp-generator project added
    Signed-off-by: Mathieu De Haeck <mathieu.de.haeck@gmail.com>

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('aemDashboard')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('aemDashboard', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
