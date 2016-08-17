b682137fdce4f6acba66730f48e1e7388425fe14
Mathieu De Haeck <mathieu.de.haeck@gmail.com>
2016-08-17 16:44:28 +0200

    removed toaster config
    Signed-off-by: Mathieu De Haeck <mathieu.de.haeck@gmail.com>

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -9,13 +9,6 @@
   function config($logProvider, toastrConfig) {
     // Enable log
     $logProvider.debugEnabled(false);
-
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = true;
-    toastrConfig.progressBar = true;
   }
 
 })();
